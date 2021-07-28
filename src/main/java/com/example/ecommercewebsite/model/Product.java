b92cc567b9f7a8b441172ed4eaddabf8f1927044
danamusetescu <danactt27@gmail.com>
2021-07-28 12:12:17 +0300

    product entity, fields, getters and setters and relationships

---
+++ src/main/java/com/example/ecommercewebsite/model/Product.java
@@ -0,0 +1,57 @@
+package com.example.ecommercewebsite.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    private Long id;
+    private byte[] image;
+    private Double price;
+    private String description;
+    private String shortDescription;
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+}
