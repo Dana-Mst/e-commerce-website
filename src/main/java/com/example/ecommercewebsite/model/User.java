76b221d39aa3d77c0f7c05caffd1efbbad64d93d
danamusetescu <danactt27@gmail.com>
2021-07-28 12:56:10 +0300

    user entity, fields, getters and setters and relationships

---
+++ src/main/java/com/example/ecommercewebsite/model/User.java
@@ -0,0 +1,79 @@
+package com.example.ecommercewebsite.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    private Long id;
+    private String role;
+    private String password;
+    private String email;
+    private Set<Product> products = new HashSet<>();
+    private Set<Review> reviews = new HashSet<>();
+    private Cart cart;
+
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @OneToMany
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+        public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    @OneToMany
+    public Set<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(Set<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    @OneToOne
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+}
