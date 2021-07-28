f91aaecdf2d385ebc41e0a5928ce6996846ef1d1
danamusetescu <danactt27@gmail.com>
2021-07-28 13:00:29 +0300

    review entity, fields, getters and setters and relationships

---
+++ src/main/java/com/example/ecommercewebsite/model/Review.java
@@ -0,0 +1,68 @@
+package com.example.ecommercewebsite.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Review {
+
+    private Long id;
+    private String text;
+    private Integer rating;
+    private Date date;
+    private Product product;
+    private User user;
+
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @ManyToOne
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @ManyToOne
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
